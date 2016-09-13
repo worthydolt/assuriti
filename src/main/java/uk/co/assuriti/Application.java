ec8daf4dc2e38ce4522f1ce5c840506ec555ea7e
worthydolt <neil.frow@gmail.com>
2016-09-13 19:26:46 +0100

    Brings in Camunda deps and adds a Spring Boot runnable class

---
+++ src/main/java/uk/co/assuriti/Application.java
@@ -0,0 +1,15 @@
+package uk.co.assuriti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by neil on 13/09/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+         SpringApplication.run(Application.class, args);
+    }
+    }
